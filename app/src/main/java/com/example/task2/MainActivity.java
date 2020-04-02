792bd174f0b00f190d3925d846aeec60df4a7eb0
ENGR. ABIODUN <fowobaje@gmail.com>
2020-04-02 22:07:50 +0100

    TASK2

---
+++ app/src/main/java/com/example/task2/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.task2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
